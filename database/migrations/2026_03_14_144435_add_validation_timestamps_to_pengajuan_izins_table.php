9fa15b5cc0472c43c6d0e244596db9f792b5995e
Jessvpb <160694331+Jessvpb@users.noreply.github.com>
2026-03-14 14:48:10 +0700

    Update 2026_03_14_144435_add_validation_timestamps_to_pengajuan_izins_table.php

--- database/migrations/2026_03_14_144435_add_validation_timestamps_to_pengajuan_izins_table.php
+++ database/migrations/2026_03_14_144435_add_validation_timestamps_to_pengajuan_izins_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('pengajuan_izins', function (Blueprint $table) {
+        Schema::table('pengajuan_izin', function (Blueprint $table) {
             // Menambahkan kolom untuk mencatat waktu persetujuan masing-masing
             $table->timestamp('tgl_validasi_kepala')->nullable()->after('validasi_kepalacabang');
             $table->timestamp('tgl_validasi_admin')->nullable()->after('validasi_admin');
@@ -20,7 +20,7 @@ return new class extends Migration
 
     public function down()
     {
-        Schema::table('pengajuan_izins', function (Blueprint $table) {
+        Schema::table('pengajuan_izin', function (Blueprint $table) {
             $table->dropColumn(['tgl_validasi_kepala', 'tgl_validasi_admin']);
         });
     }
